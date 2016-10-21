07f2bf4366c362c58b74f2419d5d97cc548836df
liugxiami <xiamiliu@gmail.com>
2016-10-20 21:28:50 -0500

    Update Main.java

--- src/com/ccsi/Main.java
+++ src/com/ccsi/Main.java
@@ -22,4 +22,22 @@ public class Main {
         chars[p]=chars[q];
         chars[q]=temp;
     }
+	
+     public static String reverseString(String s){
+        if(s==null||s.length()==0)return s;
+        String res="";
+        while(s.length()!=0){
+            res=s.charAt(0)+res;
+            s=s.substring(1);
+        }
+        return res;
+    }
+    public static String reverseString1(String s){
+        if(s==null||s.length()==0)return s;
+        StringBuilder sb=new StringBuilder();
+        for (int i = s.length()-1; i >=0 ; i--) {
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
 }
